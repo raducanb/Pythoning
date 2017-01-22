e6f0a9d65bf1de8359edf6c37752002cd5b165fd
Bogdan Raducan <bogdan.raducan@emag.ro>
2017-01-22 23:49:29 +0200

    Stahdee

--- l/l2.py
+++ l/l2.py
@@ -12,3 +12,41 @@ def fibonacci_list(n):
 
 print(fibonacci_list(10))
 
+#5
+# def combinatios_of_len(list_c, len_k):
+#     combinations = list()
+#     for i in range(0, len_k):
+#
+# print(tuple("ABCD"))
+        
+#6
+class Counter(dict):
+    def __missing__(self, key):
+        return 0
+
+def elements_that_appear_x_times(times, *lists):
+    elements_dict = Counter()
+    for a_list in lists:
+        for a_element in a_list:
+            elements_dict[a_element] += 1
+    return list({k:v for k, v in elements_dict.items() if v == times}.keys())
+    
+print(elements_that_appear_x_times(2, [1,2,3], [2,3,4], [4,5,6], [4,1,"test"]))
+
+#test
+a_list = [1, 9, 8, 4]
+print([elem * 2 for elem in a_list])
+print(list(map(lambda x:x*2, a_list)))
+for x in "abcd":
+    print(ord(x))
+
+#8
+def tuples_lists_from_lists(*input_lists):
+    max_len = max([len(x) for x in input_lists])
+    output_lists = []
+
+    for x in range(0, max_len):
+        output_lists.insert(x, ([input_list[x] if len(input_list) > x else None for input_list in input_lists]))
+    return output_lists
+
+print(tuples_lists_from_lists((1,2,3),(4,5,6),(7,8)))
